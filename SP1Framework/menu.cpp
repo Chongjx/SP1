f80af647625423a3fd09d50622e9620481dbb8fc
Nicholas Lum Ye Han <nlyh_97@hotmail.com>
2014-08-20 11:27:12 +0800

    added instruction and stuff

--- SP1Framework/menu.cpp
+++ SP1Framework/menu.cpp
@@ -14,20 +14,23 @@ int mainmenu()
     {
 		int choice = 0;
 		cls();
-        cout <<     "                            ______                       __                        " << endl;
-        cout <<     "                           /      \\                     |  \\                     " << endl;
-        cout <<     "                          |  SSSSSS\\ _______    ______  | kk   __   ______        " << endl;
-        cout <<     "                          | SS___\\SS|       \\  |      \\ | kk  /  \\ /      \\   " << endl;
-        cout <<     "                           \\SS    \\ | nnnnnnn\\  \\aaaaaa\\| kk_/  kk|  eeeeee\\ " << endl;
-        cout <<     "                           _\\SSSSSS\\| nn  | nn /      aa| kk   kk | ee    ee     " << endl;
-        cout <<     "                          |  \\__| SS| nn  | nn|  aaaaaaa| kkkkkk\\ | eeeeeeee     " << endl;
-        cout <<     "                           \\SS    SS| nn  | nn \\aa    aa| kk  \\kk\\ \\ee     \\ " << endl;
-        cout <<     "                            \\SSSSSS  \\nn   \\nn  \\aaaaaaa \\kk   \\kk  \\eeeeeee" << endl << endl;
-		cout <<     "                                           1) - Play" << endl;
-		cout <<     "                                           2) - Instruction" << endl;
-		cout <<     "                                           3) - Highscore" << endl;
-		cout <<     "                                           4) - Exit" << endl << endl;
-		cout <<     "                                           Please choose an option: ";
+        cout <<     "                       ******************************************************" << endl;
+        cout <<     "                       *   ______                       __                  *" << endl;
+        cout <<     "                       *  /      \\                     |  \\                 *" << endl;
+        cout <<     "                       * |  SSSSSS\\ _______    ______  | kk   __   ______   *" << endl;
+        cout <<     "                       * | SS___\\SS|       \\  |      \\ | kk  /  \\ /      \\  *" << endl;
+        cout <<     "                       *  \\SS    \\ | nnnnnnn\\  \\aaaaaa\\| kk_/  kk|  eeeeee\\ *" << endl;
+        cout <<     "                       *  _\\SSSSSS\\| nn  | nn /      aa| kk   kk | ee    ee *" << endl;
+        cout <<     "                       * |  \\__| SS| nn  | nn|  aaaaaaa| kkkkkk\\ | eeeeeeee *" << endl;
+        cout <<     "                       *  \\SS    SS| nn  | nn \\aa    aa| kk  \\kk\\ \\ee     \\ *" << endl;
+        cout <<     "                       *   \\SSSSSS  \\nn   \\nn  \\aaaaaaa \\kk   \\kk  \\eeeeeee *" << endl; 
+        cout <<     "                       *                                                    *" << endl;
+        cout <<     "                       ******************************************************" << endl << endl;
+		cout <<     "                                         1) - Play" << endl;
+		cout <<     "                                         2) - Instruction" << endl;
+		cout <<     "                                         3) - Highscore" << endl;
+		cout <<     "                                         4) - Exit" << endl << endl;
+		cout <<     "                                         Please choose an option: ";
 
 		cin >> choice;
 
@@ -48,8 +51,17 @@ void instruction()
 {
 	cls();
 	char option = 0;
-	cout << "Instructions go here" << endl;
-	cout << "Press any key to return to the main menu!" << endl;
+	cout << "                          _           _                   _   _                  " << endl;
+    cout << "                         (_)_ __  ___| |_ _ __ _   _  ___| |_(_) ___  _ __  ___  " << endl;
+    cout << "                         | | '_ \\/ __| __| '__| | | |/ __| __| |/ _ \\| '_ \\/ __| " << endl;
+    cout << "                         | | | | \\__ \\ |_| |  | |_| | (__| |_| | (_) | | | \\__ \\ " << endl;
+    cout << "                         |_|_| |_|___/\\__|_|   \\__,_|\\___|\\__|_|\\___/|_| |_|___/ " << endl << endl;
+    cout << "                                               How to play?" << endl << endl;
+    cout << "                         Use the arrow keys to direct the movement of the snake."<< endl; 
+    cout << "                     Eat more food produced randomly in the map to progress further."<< endl;
+    cout << "                         The game gets more challenging as the snake gets longer."<< endl; 
+    cout << "               However,the snake will die if it touches its own body or touches the walls."<< endl << endl;
+    cout << "                                Press any key to return to the main menu!" << endl;
 	cin >> option;
 }
 
@@ -80,7 +92,7 @@ void quitGame()
     cout << "            O   .oOOo O       o O       o  o      O        o     `O    `O'     O       " << endl;
     cout << "            o.      O o       O o       O  O      o        O      o     o      o       " << endl;
     cout << "             O.    oO `o     O' `o     O'  o    .O'        o     .O     O      O       " << endl;
-    cout << "              `OooO'   `OoooO'   `OoooO'   OooOO'          `OooOO'      O      ooOooOoO" << endl << endl;
+    cout << "              `OooO'   `OoooO'   `OoooO'   OooOO'          `OooOO'      O      ooOooOoO" << endl << endl << endl << endl << endl;
     cout << "                                    ";
 	g_quitGame = true;
 }
