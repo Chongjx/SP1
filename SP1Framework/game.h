3b3afe02737a87a22ecc64c01a0644b3f23e63dc
Nicholas Lum Ye Han <nlyh_97@hotmail.com>
2014-08-22 10:54:42 +0800

    Multiplayer (sort of?)

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -12,6 +12,11 @@ struct snake
 	COORD charLocation;
 };
 
+struct snake2
+{
+	COORD charLocation;
+};
+
 enum Keys
 {
     K_UP,
@@ -42,15 +47,25 @@ enum Sequence
 };
 
 void init();					// initialize your variables, allocate memory, etc
+<<<<<<< Updated upstream
 void getInput();				// get input from player
 int update(double dt);			// update the game and the state of the game
 void render();					// renders the current state of the game to the console
+=======
+void getInput();	  // get input from player 1		
+//void getInput2();   // get input from player 2
+void update(double dt);			// update the game and the state of the game
+//void update2(double dt);  // update the game and the state of the game for snake 2
+void render();	 // renders the current state of the game to the console
+//void render2();  // renders the current state of the game to the console for snake 2
+>>>>>>> Stashed changes
 void shutdown();				// do clean up, free memory
 void map();						// border around the map using 2D array
 void spawn();					// random food spawn
 void gameLoop();				// The loop
 void hiscore(int);				// Highscore system
 void checkcollision();
+//void checkcollision2();
 
 void mainmenu();					// Main menu screen
 void instruction();				// Into the instruction for user
@@ -58,7 +73,14 @@ void game();					// Into the game
 void highscore();				// Into the highscore
 void quitGame();				// Quit the game
 void createsnake(int size);		// Create my snake at the intial location
+<<<<<<< Updated upstream
 int updatesnake();
+=======
+//void createsnake2(int size);
+void updatesnake();
+//void updatesnake2();
+>>>>>>> Stashed changes
 void gg();
+//void gg2();
 
 #endif // _GAME_H
