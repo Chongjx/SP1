258740b60370db54e43b615347bd455a96ebfeb9
ChongJx <chonjjx60@gmail.com>
2014-08-22 12:48:41 +0800

    nothing

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -19,12 +19,12 @@ struct snake2
 
 enum Keys
 {
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    K_COUNT
+	K_UP,
+	K_DOWN,
+	K_LEFT,
+	K_RIGHT,
+	K_ESCAPE,
+	K_COUNT
 };
 
 enum Colours
@@ -55,18 +55,15 @@ enum Sequence
 };
 
 void init();					// initialize your variables, allocate memory, etc
-<<<<<<< Updated upstream
 void getInput();				// get input from player
 int update(double dt);			// update the game and the state of the game
 void render();					// renders the current state of the game to the console
-=======
-void getInput();	  // get input from player 1		
-//void getInput2();   // get input from player 2
-void update(double dt);			// update the game and the state of the game
-//void update2(double dt);  // update the game and the state of the game for snake 2
-void render();	 // renders the current state of the game to the console
-//void render2();  // renders the current state of the game to the console for snake 2
->>>>>>> Stashed changes
+void getInput();				// get input from player 1		
+//void getInput2();				// get input from player 2
+int update(double dt);			// update the game and the state of the game
+//void update2(double dt);		// update the game and the state of the game for snake 2
+void render();					// renders the current state of the game to the console
+//void render2();				// renders the current state of the game to the console for snake 2
 void shutdown();				// do clean up, free memory
 void map();						// border around the map using 2D array
 void spawn();					// random food spawn
@@ -75,25 +72,17 @@ void hiscore(int);				// Highscore system
 void checkcollision();
 //void checkcollision2();
 
-void mainmenu();					// Main menu screen
+void mainmenu();				// Main menu screen
 void instruction();				// Into the instruction for user
 void game();					// Into the game
 void highscore();				// Into the highscore
 void options();
 void quitGame();				// Quit the game
 void createsnake(int size);		// Create my snake at the intial location
-<<<<<<< Updated upstream
 int updatesnake();
-=======
 //void createsnake2(int size);
-void updatesnake();
 //void updatesnake2();
->>>>>>> Stashed changes
 void gg();
-<<<<<<< HEAD
 void colourOptions();
-=======
-//void gg2();
->>>>>>> origin/master
 
 #endif // _GAME_H
